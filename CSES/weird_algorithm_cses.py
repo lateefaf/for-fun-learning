5561baf07ac4566837027bec03cd7947ad43f8a1
Lateef <alateef4@gmail.com>
2024-03-14 06:34:12 -0400

    Intial commit

---
+++ CSES/weird_algorithm_cses.py
@@ -0,0 +1,13 @@
+def algo(n):
+    s = ""
+    while n != 1:
+        s += f"{int(n)} "
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = n * 3
+            n += 1
+    return s
+
+
+print(algo(3))
